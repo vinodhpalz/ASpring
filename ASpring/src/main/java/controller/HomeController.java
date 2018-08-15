8fe33ac8de8e42701ee6355d04fe1df659500e0e
vinodhpl <31344189+vinodhpl@users.noreply.github.com>
2018-08-15 13:42:43 +0530

    files added

--- ASpring/src/main/java/controller/HomeController.java
+++ ASpring/src/main/java/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//controller comment
 @Controller
 public class HomeController {
 
